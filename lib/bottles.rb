83108e51f6c36c11d5eb3dd87edaeb675f2bdfaa
David Katz <15Dkatz@shcp.edu>
2018-07-12 13:25:13 -0700

    Pass 1 test in initial exercise

---
+++ lib/bottles.rb
@@ -0,0 +1,8 @@
+class Bottles
+  def verse(number)
+    [
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n",
+      "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    ].join()
+  end
+end
